9a10fb436e9027887381f5101385df9f72b99972
Rahul Singh <rahulsingh793@gmail.com>
2021-12-30 15:35:08 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Craftveda Technologies on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
